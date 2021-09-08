85a13a9fc797242be50719c09e65a52d8f847a16
Masaki Kawabata Neto <kawabata@usp.br>
2021-09-08 10:42:30 -0300

    organizando pagina inicial

---
+++ resources/views/partials/badge-grupo-sem-coluna.blade.php
@@ -0,0 +1,3 @@
+@if ($grupo->coluna > config('portal-sistemas.num_cols'))
+  <span class="badge badge-danger ml-2">Grupo sem coluna</span>
+@endif
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,40 +3,17 @@
 @section('content')
 
   <div class="row">
-    @foreach (range(1,config('portal-sistemas.num_cols')) as $col)
+    @foreach (range(1, config('portal-sistemas.num_cols')) as $col)
       <div class="col-md-{{ config('portal-sistemas.col_width') }}">
-        @foreach ($grupos->where('coluna', $col) as $grupo)
-          <div class="card mb-3">
-            <div class="card-header h4">
-              {{ $grupo->nome }}
-            </div>
-            <div class="card-body">
-              @foreach ($grupo->sistemas as $sistema)
-                @include('partials.sistema')
-              @endforeach
-            </div>
-          </div>
-        @endforeach
+        @include('partials.grupos-coluna')
       </div>
     @endforeach
   </div>
-  <hr>
+
   <div class="row">
-    @foreach (range(config('portal-sistemas.num_cols')+1, 4) as $col)
-      <div class="col-md-{{ config('portal-sistemas.col_width') }}">
-        @foreach ($grupos->where('coluna', $col) as $grupo)
-          <div class="card mb-3">
-            <div class="card-header">
-              <span class="h4 align-middle">{{ $grupo->nome }}</span> 
-              @can('admin')<span class="badge badge-danger ml-2">Grupo sem coluna</span>@endcan
-            </div>
-            <div class="card-body">
-              @foreach ($grupo->sistemas as $sistema)
-                @include('partials.sistema')
-              @endforeach
-            </div>
-          </div>
-        @endforeach
+    @foreach (range(config('portal-sistemas.num_cols') + 1, 4) as $col)
+      <div class="col-md-12">
+        @include('partials.grupos-coluna')
       </div>
     @endforeach
   </div>
